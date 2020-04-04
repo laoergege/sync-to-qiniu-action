07f009edc4d417c1ee5f336bb361b7a20b3ce11d
lianyuansheng <lianyuansheng@xiao100.com>
2020-04-05 00:06:27 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const core = require('@actions/core');
-const diff = require('./src/diff')
-const Quniu = require('./src/qiniu')
+const diff = require('./diff')
+const Quniu = require('./qiniu')
 const { getInput, getWorkspace } = require('./input-helper')
 
 async function run() {
