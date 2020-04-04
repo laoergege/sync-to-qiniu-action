d94d80229b6e7e87ac5925e1cb63e4882dd8fd4b
lianyuansheng <lianyuansheng@xiao100.com>
2020-04-04 23:03:18 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -13,48 +13,43 @@ async function run() {
   const { accessKey, secretKey, bucket, zone,
     fsizeLimit, mimeLimit } = getInput()
 
-  try {
-    const qiniu = new Quniu(accessKey, secretKey, bucket, zone, {
-      fsizeLimit,
-      mimeLimit
-    })
+  const qiniu = new Quniu(accessKey, secretKey, bucket, zone, {
+    fsizeLimit,
+    mimeLimit
+  })
 
-    const summary = await diff()
+  const summary = await diff()
 
-    const op = {
-      A: [],
-      D: [],
-      M: [],
-      R: []
-    }
-
-    for (let index = 0; index < summary.length; index++) {
-      const [status, o, d] = summary[index];
-      op[status].push([o, d])
-    }
-
-    // 新增
-    const adds = op.A
-    for (let index = 0; index < adds.length; index++) {
-      const [fs] = adds[index];
-      qiniu.uploadFile(fs, path.resolve(__dirname, fs))
-    }
-
-    // 删除
-    const dels = op.D
-    qiniu.batchDelFiles(dels.map(fs => fs))
-
-    // 重命名
-    const renames = op.R
-    qiniu.batchMVFiles(renames.map(fs => fs))
+  const op = {
+    A: [],
+    D: [],
+    M: [],
+    R: []
   }
-  catch (error) {
-    core.setFailed(error.message);
+
+  for (let index = 0; index < summary.length; index++) {
+    const [status, o, d] = summary[index];
+    op[status].push([o, d])
   }
+
+  const adds = op.A
+  for (let index = 0; index < adds.length; index++) {
+    const [fs] = adds[index];
+    qiniu.uploadFile(fs, path.resolve(githubWorkspacePath, fs))
+  }
+
+  const dels = op.D
+  qiniu.batchDelFiles(dels)
+
+  const renames = op.R
+  qiniu.batchMVFiles(renames)
+
+  const modifies = op.M
+  qiniu.batchUpFiles(modifies)
 }
 
 if (core.getState("isPost")) {
-  run() 
+  run()
 } else {
   core.saveState("isPost", true);
 }
