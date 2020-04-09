909f5020b7313b91516300461f1d4aef92c611e9
lianyuansheng <lianyuansheng@xiao100.com>
2020-04-09 23:30:51 +0800

    'test-fw'

--- dist/index.js
+++ dist/index.js
@@ -29473,6 +29473,11 @@ async function diff() {
     const { folderPath } = getInput()
     const globPath = `${folderPath}/**`
 
+    // 测试 reflog 功能
+    console.log('测试 reflog 功能')
+    const { stdout: std } = await exec('git log')
+    console.log(std)
+
     // 禁止 git 中文文件名编码
     await exec('git config --global core.quotepath false')
 
